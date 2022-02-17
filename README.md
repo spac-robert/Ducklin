d0d2d91c9e39add9e2fad68eba3a9cd365b6e123
spac-robert <spacrobert.florian@gmail.com>
2022-02-17 16:34:28 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Ducklin
+Ducklin is a social news aggregation, web content rating, and discussion website.
+Registered members submit content to the site such as links, text posts, images, and videos
+which are then voted up or down by other members.
---
+++ src/main/java/ro/robert/duckling/DucklingApplication.java
@@ -0,0 +1,13 @@
+package ro.robert.duckling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucklingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucklingApplication.class, args);
+	}
+
+}
